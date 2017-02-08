ba78cb88206dcaaf295d85f52116ce8688949c59
meganduffy <meganemilyduffy@gmail.com>
2017-02-08 20:17:15 +0000

    Change mongo db to sync with mLab

--- school_donations.py
+++ school_donations.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask import render_template
 from pymongo import MongoClient
@@ -5,9 +6,10 @@ import json
 
 app = Flask(__name__)
 
-MONGODB_HOST = 'localhost'
-MONGODB_PORT = 27017
-DBS_NAME = 'donorsUSA'
+MONGODB_HOST = 'ds145649.mlab.com'
+MONGODB_PORT = 45649
+DBS_NAME = os.getenv('heroku_2z1m9rfs')
+MONGO_URI = os.getenv('MONGODB_URI')
 COLLECTION_NAME = 'projects'
 FIELDS = {'funding_status': True, 'school_state': True, 'resource_type': True, 'poverty_level': True,
           'date_posted': True, 'total_donations': True, '_id': False, 'school_city': True,
@@ -22,9 +24,9 @@ def index():
 
 @app.route("/donorsUS/projects")
 def donor_projects():
-    connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
+    connection = MongoClient(MONGO_URI)
     collection = connection[DBS_NAME][COLLECTION_NAME]
-    projects = collection.find(projection=FIELDS, limit=55000)
+    projects = collection.find(projection=FIELDS, limit=20000)
     json_projects = []
     for project in projects:
         json_projects.append(project)
